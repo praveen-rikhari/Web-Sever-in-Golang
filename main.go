92680e7dfdb807c1ab40590a27e4f64f65653784
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-06 23:54:48 +0530

    Handling POST Request

--- main.go
+++ main.go
@@ -21,12 +21,28 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful\n")
+
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
 func main() {
 	fmt.Println("Creating Web Server in Golang")
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Println("Starting server at port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {

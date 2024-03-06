e40cda8efe87f9b4058afbc3e463884b027c7aa9
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-06 21:20:41 +0530

    adding routes

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 func main() {
 	fmt.Println("Creating Web Server in Golang")
 
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, Batman 🦇")
+	})
+
 	fmt.Println("Starting server at port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)

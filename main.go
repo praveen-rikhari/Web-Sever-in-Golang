7933cf343c6adeb3a1c3d5c3c6c5f81e77c70e85
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-06 20:33:37 +0530

    Creating & starting the webserver

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Creating Web Server in Golang")
+
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }

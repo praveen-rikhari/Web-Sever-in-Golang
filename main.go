2b84d322b1d2ff9d1a5faa780662bc625515eea0
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-06 21:46:43 +0530

    Adding basic security

--- main.go
+++ main.go
@@ -6,12 +6,25 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found ❌", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello, Batman 🦇")
+
+}
+
 func main() {
 	fmt.Println("Creating Web Server in Golang")
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Batman 🦇")
-	})
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Println("Starting server at port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
